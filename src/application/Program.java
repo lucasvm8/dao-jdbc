dea8ab43aa41768ff38eb8a0c507cb7e30d9c00f
Lucas Vasconcelos <lucasvm8@gmail.com>
2022-01-23 00:03:47 -0300

    Interfaces DAO deenvolvidas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,47 @@
 package application;
 
+import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1,"books");
+         Department obj = new Department(1,"books");
 
-        System.out.println(obj);
+         Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+         Connection con = null;
+         Statement st = null;
+         ResultSet rs = null;
+
+         try{
+             con = DB.getConnection();
+             st = con.createStatement();
+
+             rs = st.executeQuery("SELECT * FROM seller");
+
+             while(rs.next()){
+                 System.out.println(rs.getInt("id"));
+             }
+
+         }
+         catch(SQLException e){
+            e.printStackTrace();
+        }
+         finally {
+             DB.closeConnection();
+             DB.closeStatement(st);
+         }
+
+        System.out.println(seller);
 
     }
 
