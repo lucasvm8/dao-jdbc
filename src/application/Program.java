2dd3fdfdb5029e9a0f5b9dcfc1658a8e8b01c73a
Lucas Vasconcelos <lucasvm8@gmail.com>
2022-02-03 02:08:35 -0300

    CRUD com banco de dados finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.DAO.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -13,7 +14,7 @@ public class Program {
     public static void main(String[] args) {
 
        SellerDAO sellerDAO = DaoFactory.createSellerDAO();
-
+/*
        Seller seller = sellerDAO.findById(3);
        System.out.println(seller);
 
@@ -24,6 +25,29 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println();
+
+        list = sellerDAO.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+ */
+        Department dp = new Department(2, null);
+
+/*
+        Seller seller = new Seller(null, "greg", "greg@gmail.com", new Date(), 4.000, dp);
+        sellerDAO.insert(seller);
+        System.out.println("Inserted new id = " + seller.getId());
+        */
+
+        Seller sl = sellerDAO.findById(1);
+        sl.setName("Bob Base Brown");
+        sellerDAO.update(sl);
+        System.out.println("update completed");
+
+        sellerDAO.deleteById(7);
+
     }
 
 }
